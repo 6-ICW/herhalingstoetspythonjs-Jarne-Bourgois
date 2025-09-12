d3c0ea5b0e5ae760358be1a6bf6135a20a54ef30
jarnebourgois <Jarne.Bourgois@student.tsaam.be>
2025-09-12 10:20:16 +0200

    commit Laatste

--- htmlJS/script/script.js
+++ htmlJS/script/script.js
@@ -2,11 +2,21 @@
 // deleteBtn.textContent = "🗑️"; --> maakt als inhoud een vuilbakje
 
 // schrijf je code om de volledige todo te doen werken
-
+const txtToDo = document.getElementById("todo-input");
+const todoList = document.getElementById("todo-list");
+const btnAdd = document.getElementById("add-btn");
 // algemene variabelen en verwijzingen
 
 // toevoegen van een todo
-
+btnAdd.addEventListener("click", () => {
+  let todo = document.createElement("li");
+  todo.setAttribute("id ");
+  let tekst = document.createElement("p");
+  tekst.innerText = txtToDo.value;
+  todo.appendChild(tekst);
+  todoList.appendChild(todo);
+  txtToDo.value = "";
+});
 // afvinken van een todo
 
 // verwijderen van een todo
